706a1985d625e3bb0f5c9bdbddac0f9e9e656216
Dmitro Dohryk <dmitro.dohryk@gmail.com>
2023-05-19 13:56:21 +0300

    Initial Commit

---
+++ StoreApp/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Dmitro Dohryk on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
